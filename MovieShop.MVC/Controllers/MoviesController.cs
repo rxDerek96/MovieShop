ae3d6ccb12292aaea9f7b86406a25b1731405e90
Your Name <you@example.com>
2021-06-02 14:24:00 -0700

    First commit

---
+++ MovieShop.MVC/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+        public IActionResult TopRatedMovies()
+        {
+            return View();
+        }
+    }
+}
